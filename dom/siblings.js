beb21b6f1c10661d791f811f37317b76f174c80d
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-02-21 11:40:48 -0300

    siblings

---
+++ dom/siblings.js
@@ -0,0 +1,10 @@
+const el = document.querySelector('header');
+
+// nextSibling nextElementSibling
+console.log(el.nextSibling);
+console.log(el.nextElementSibling);
+
+// previousSibling previousElementSibling
+const el1 = document.querySelector('body script');
+console.log(el.previousSibling);
+console.log(el.previousElementSibling);
