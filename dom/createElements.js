15d71a41423834778775df1f8791668a2beea3c5
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-02-21 11:59:15 -0300

    insertBefore

--- dom/createElements.js
+++ dom/createElements.js
@@ -7,3 +7,13 @@ const body = document.querySelector('body');
 body.append(div);
 
 body.prepend(div);
+
+const newElement = document.createElement('h2');
+newElement.innerText = 'New Element'
+const script = body.querySelector('script');
+body.insertBefore(newElement, script);
+
+// insert after an element
+const heading3 = document.createElement('h3');
+heading3.innerText = 'Heading 3';
+body.insertBefore(heading3, div.nextElementSibling);
