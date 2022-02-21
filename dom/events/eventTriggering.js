fe9d00343b7305f1094a1b86a1981789abf0d6a2
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-02-21 14:07:01 -0300

    event triggering

---
+++ dom/events/eventTriggering.js
@@ -0,0 +1,19 @@
+const h1 = document.querySelector('h1');
+h1.style.color = "navy";
+h1.style.transition = "0.1s";
+
+h1.addEventListener('click', print);
+h1.addEventListener('mouseover', mouseOver);
+h1.addEventListener('mouseleave', mouseLeave);
+
+function print() {
+  console.log('clicked');
+}
+
+function mouseOver() {
+  h1.style.color = "gold";
+}
+
+function mouseLeave() {
+  h1.style.color = "navy";
+}
