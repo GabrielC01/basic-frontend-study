a8c86d8c304271fd3a48bc9ee7fe8f481067deaf
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-02-21 11:08:11 -0300

    style handling

---
+++ dom/styles.js
@@ -0,0 +1,5 @@
+const body = document.querySelector('body');
+const h1 = document.querySelector('h1');
+h1.style.color = 'white';
+h1.style.textShadow = "3px 3px 3px lightgray";
+body.style.backgroundColor = "#feece8";
