a7bfb6c211723261b2fe79bacaaee44f9175230d
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-02-21 11:53:06 -0300

    crete elements

---
+++ dom/createElements.js
@@ -0,0 +1,9 @@
+// createElement
+const div = document.createElement('div');
+div.innerText = 'Olá devs';
+
+// append prepend
+const body = document.querySelector('body');
+body.append(div);
+
+body.prepend(div);
