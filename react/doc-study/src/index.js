38bbb04d37350062b4923a4d6dfee03386acedf3
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-07 10:38:54 -0300

    Components.

--- react/doc-study/src/index.js
+++ react/doc-study/src/index.js
@@ -3,21 +3,16 @@ import ReactDOM from 'react-dom';
 
 const rootElement = document.getElementById('root');
 
-/* Updating rendered elements */
-// React elements are immutable, so, in order to update the UI,
-// it's necessary to create new elements.
+/* Components and props */
 
-function tick() {
-	const element = (
-		<div>
-			<h1>Hello, user!</h1>
-			<h2>It is {new Date().toLocaleTimeString()}.</h2>
-		</div>
-	);
-	ReactDOM.render(element, rootElement);
+// Function component:
+function Welcome(props) {
+	return <h1>Hello, {props.name}!</h1>;
 }
 
-// React only updates the necessary, by comparing the previous
-// state to the current one.
-
-setInterval(tick, 1000);
+// Class component:
+class Welcome extends React.Component {
+	render() {
+		return <h1>Hello, {props.name}!</h1>;
+	}
+}
