81e5fbfea167784d7005f8dcfb516343dd23bb1a
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-07 10:42:59 -0300

    Rendering components.

--- react/doc-study/src/index.js
+++ react/doc-study/src/index.js
@@ -5,14 +5,12 @@ const rootElement = document.getElementById('root');
 
 /* Components and props */
 
-// Function component:
 function Welcome(props) {
 	return <h1>Hello, {props.name}!</h1>;
 }
 
-// Class component:
-class Welcome extends React.Component {
-	render() {
-		return <h1>Hello, {props.name}!</h1>;
-	}
-}
+// Rendering components:
+const element = <Welcome name="Gabriel" />;
+ReactDOM.render(element, rootElement);
+
+// Obs: Always start component names with capital letters.
