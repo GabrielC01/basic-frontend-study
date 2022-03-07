ed369fa9853bf6c2a2bbf49486f018c2e091db33
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-07 09:17:53 -0300

    JS expressions inside JSX

--- react/doc-study/src/index.js
+++ react/doc-study/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const rootElement = document.getElementById('root');
-const element = <h1>JSX</h1>;
+
+const name = 'Gabriel';
+const newElement = <h1>Hello, {name}!</h1> // JSX can contain JS expressions
 
 ReactDOM.render(
-	element,
+	newElement,
 	rootElement
 );
