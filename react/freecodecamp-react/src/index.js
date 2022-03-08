eef3a196d4f7896090881472a97f749efd79714c
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-08 11:19:56 -0300

    Access props using this.props.

--- react/freecodecamp-react/src/index.js
+++ react/freecodecamp-react/src/index.js
@@ -4,25 +4,30 @@ import PropTypes from 'prop-types';
 
 const root = document.getElementById('root');
 
-const Items = (props) => {
-	return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
-};
-
-Items.propTypes = {
-	quantity: PropTypes.number.isRequired
-};
-
-Items.defaultProps = {
-	quantity: 0
-};
-
-class ShoppingCart extends React.Component {
+class App extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 	render() {
-		return <Items quantity={10} />
+		return (
+			<div>
+				<Welcome name="user" />
+			</div>
+		);
 	}
 }
 
-ReactDOM.render(<ShoppingCart />, root);
+class Welcome extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				<p>Hello, <strong>{this.props.name}</strong>!</p>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, root);
