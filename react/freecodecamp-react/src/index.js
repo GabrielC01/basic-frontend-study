5e2b4a3f7b35bd125ce05d35f844eae5b7f0be0a
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-08 09:37:31 -0300

    Add class to element.

--- react/freecodecamp-react/src/index.js
+++ react/freecodecamp-react/src/index.js
@@ -4,9 +4,8 @@ import ReactDOM from 'react-dom';
 const root = document.getElementById('root');
 
 const JSX = (
-	<div>
-		<h1>Hello, user!</h1>
-		<p>Lets render this to the DOM.</p>
+	<div className="myDiv">
+		<h1>Add a class to this div</h1>
 	</div>
 );
 
