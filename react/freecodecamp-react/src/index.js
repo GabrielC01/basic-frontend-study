cb6a51f063c2aed81afbdbc05201a0b40ffc5519
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-08 10:58:47 -0300

    Pass array as property.

--- react/freecodecamp-react/src/index.js
+++ react/freecodecamp-react/src/index.js
@@ -3,26 +3,25 @@ import ReactDOM from 'react-dom';
 
 const root = document.getElementById('root');
 
-const CurrentDate = (props) => {
-	return (
-		<div>
-			<p> The current date is: {props.date}</p>
-		</div>
-	);
-}
+const List = (props) => {
+	return <p>{props.tasks.join(', ')}</p>;
+};
 
-class Calendar extends React.Component {
+class ToDo extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 	render() {
 		return (
 			<div>
-				<h3>What date is it?</h3>
-				<CurrentDate date={Date()} />
+				<h1>To Do Lists</h1>
+				<h2>Today</h2>
+				<List tasks={["walk dog", "workout"]} />
+				<h2>Tomorrow</h2>
+				<List tasks={["walk", "whatever", "workout"]} />
 			</div>
 		);
 	}
 }
 
-ReactDOM.render(<Calendar />, root);
+ReactDOM.render(<ToDo />, root);
