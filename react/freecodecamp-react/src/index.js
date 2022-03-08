f04c94492dd6f62185ed6aeb27d82f12321520f9
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-08 11:06:13 -0300

    Change override default property.

--- react/freecodecamp-react/src/index.js
+++ react/freecodecamp-react/src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom';
 
 const root = document.getElementById('root');
 
-const ShoppingCart = (props) => {
-	return (
-		<div>
-			<h1>Shopping Cart Component</h1>
-		</div>
-	);
-}
-
-ShoppingCart.defaultProps = {
-	items: 0
+const Items = (props) => {
+	return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
 };
+
+Items.defaultProps = {
+	quantity: 0
+};
+
+class ShoppingCart extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return <Items quantity={10} />
+	}
+}
 
 ReactDOM.render(<ShoppingCart />, root);
