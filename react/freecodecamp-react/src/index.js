8845930d6e7842cd50af73dc7193aecb6d6a0a86
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-08 09:56:18 -0300

    Composing components.

--- react/freecodecamp-react/src/index.js
+++ react/freecodecamp-react/src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom';
 
 const root = document.getElementById('root');
 
-class MyComponent extends React.Component {
+const ChildComponent = () => {
+	return (
+		<div>
+			<p>I am the child</p>
+		</div>
+	);
+};
+
+class ParentComponent extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 	render() {
-		return (<div><h1>Hello React!</h1></div>);
+		return (
+			<div>
+				<h1>I am the parent</h1>
+				<ChildComponent />
+			</div>
+		)
 	}
-};
+}
 
-ReactDOM.render(<MyComponent />, root);
+ReactDOM.render(<ParentComponent />, root);
