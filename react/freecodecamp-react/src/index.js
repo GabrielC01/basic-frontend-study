cb83e3b724f0251fcc41d5e02c555d61afd02232
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-08 11:15:26 -0300

    Strict property type.

--- react/freecodecamp-react/src/index.js
+++ react/freecodecamp-react/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 const root = document.getElementById('root');
 
 const Items = (props) => {
 	return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+};
+
+Items.propTypes = {
+	quantity: PropTypes.number.isRequired
 };
 
 Items.defaultProps = {
