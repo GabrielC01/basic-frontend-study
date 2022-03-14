f48858cfedc9f04d86e4be71d3fbca7b73bdde7e
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-03-14 16:13:51 +0000

    React lists with keys.

--- react/doc-study/src/index.js
+++ react/doc-study/src/index.js
@@ -5,20 +5,23 @@ const rootElement = document.getElementById('root');
 
 /* React Lists */
 
-// Renderind list items without keys, thus
-// there's no tracking of them.
+// Now, it is possible to track the list items.
 
 function Car(props) {
 	return <li>I am a { props.brand }</li>;
 }
 
 function Garage() {
-	const cars = ['Ford', 'BMW', 'Audi'];
+	const cars = [
+		{id: 1, brand: 'Ford'},
+		{id: 2, brand: 'BMW'},
+		{id: 3, brand: 'Audi'}
+	];
 	return (
 		<>
 			<h1>Who lives in my garage?</h1>
 			<ul>
-				{cars.map((car) => <Car brand={car}/>)}
+				{cars.map((car) => <Car key={car.id} brand={car.brand}/>)}
 			</ul>
 		</>
 	);
